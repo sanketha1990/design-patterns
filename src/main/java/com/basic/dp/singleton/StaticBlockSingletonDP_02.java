367ad22489c605c5563a314584496577158659b5
sanketha1990 <sanketha.1990@gmail.com>
2021-01-03 21:53:19 +0530

    Updated warnings

--- src/main/java/com/basic/dp/singleton/StaticBlockSingletonDP_02.java
+++ src/main/java/com/basic/dp/singleton/StaticBlockSingletonDP_02.java
@@ -2,7 +2,7 @@ package com.basic.dp.singleton;
 
 public class StaticBlockSingletonDP_02 {
 
-    public static StaticBlockSingletonDP_02 instance=null;
+    public static StaticBlockSingletonDP_02 instance;
     private StaticBlockSingletonDP_02(){
         System.out.println("Private StaticBlockSingletonDP_02 constructor " +instance);
     }
--- src/main/java/com/basic/dp/stratergy/IStratergy.java
+++ src/main/java/com/basic/dp/stratergy/IStratergy.java
@@ -1,5 +1,5 @@
 package com.basic.dp.stratergy;
 
 public interface IStratergy {
-    public void actionCommand();
+    void actionCommand();
 }
